a41d6215c16aeb6ce55d09cda78137939f2ffe53
Damaris C <DCampos07@users.noreply.github.com>
2021-01-08 23:40:21 -0500

    changed about file

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,23 +1,89 @@
-import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
 function About() {
+  const items = [
+    {
+      id: 1,
+      altText: 'Slide 1',
+      caption: 'Slide 1'
+    },
+    {
+      id: 2,
+      altText: 'Slide 2',
+      caption: 'Slide 2'
+    },
+    {
+      id: 3,
+      altText: 'Slide 3',
+      caption: 'Slide 3'
+    }
+  ];
+
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        className="custom-tag"
+        tag="div"
+        key={item.id}
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+      >
+        <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
   return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-        </p>
-      </div>
-    </section>
-  );
-}
+    <div>
+      <style>
+        {
+          `.custom-tag {
+              max-width: 100%;
+              height: 500px;
+              background: black;
+            }`
+        }
+      </style>
+      <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      </Carousel>
+    </div>
+  )
+};
 
 export default About;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { IconName } from "react-icons/go";
 
 function Footer() {
     return (
         <header class="flex-row px-5">
-                    <h2>Contact Me</h2>
-                <div>
-                    <ul class="flex-row">
-                        <li class="mx-3" color="link"> 407-799-7456</li>
-                        <li class="mx-3" color="link"> <a href="dccampos00@gmail.com"></a>E-mail</li>
-                        <li class="mx-3" color="link"> <a href="https://github.com/DCampos07">Github</a></li>
-                        <li class="mx-3" color="link"> <a href="https://www.linkedin.com/in/damaris-c-870a1472">Linkedin</a></li>
-                    </ul>
-                </div>
+            <h2>Contact Me</h2>
+            <div>
+                <ul class="flex-row">
+                    <li class="mx-3" color="link"> 407-799-7456</li>
+                    <li class="mx-3" color="link"> <a href="dccampos00@gmail.com"></a>E-mail</li>
+                    <li class="mx-3" color="link"> <a href="https://github.com/DCampos07">Github</a></li>
+                    <li class="mx-3" color="link"> <a href="https://www.linkedin.com/in/damaris-c-870a1472">Linkedin</a></li>
+                </ul>
+            </div>
         </header>
     );
 };
